0c50da07fb855d7163a294bd5cb573050bb51c2a
RoyOkite <royokite@gmail.com>
2022-08-16 13:19:09 +0300

    Resubmitted Completed assignment, 2nd commit

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 //Control flow lab!
+
 function scuberGreetingForFeet(num){
   if (num <= 400) return 'This one is on me!';
   else if (num > 2000 && num <= 2500) return 'I will gladly take your thirty bucks.';
   else if (num > 2500) return 'No can do.';
-
 }
 
 function ternaryCheckCity(city){
-  if (city==='NYC') return 'Ok, sounds good.';
-  else return 'No go.';
+  return city==='NYC'? 'Ok, sounds good.': 'No go.';
 }
 
-function switchOnCharmFromTip(tip){
-  if (tip==='generous') return 'Thank you so much.';
-  else if (tip==='not as generous') return 'Thank you.'
-  else return 'Bye.';
+function switchOnCharmFromTip(message) {
+  let tip;
+  switch(message) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.'
+      break;
+  }
 }
